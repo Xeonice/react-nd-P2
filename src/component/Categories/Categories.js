6c16835a181aa75bf249b2a50577237e9f62a0a7
Xeonice <ad546971975@gmail.com>
2018-03-14 22:06:25 +0800

    add Categories

---
+++ src/component/Categories/Categories.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+import AddPost from '../Posts/PostActions/AddPost'
+
+const Categories = ({ categories }) => (
+    <nav>
+        <Link to='/'>
+            <i
+                className="material-icons"
+                style={{ color: 'green', padding: '10px' }}
+                role="button"
+                tabIndex="-1"
+            >
+                home
+            </i>
+        </Link>
+        <a className="navbar-brand"><strong>Categories</strong></a>
+        <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul>
+                {categories && categories.map(item => (
+                    <li className="nav-item" key={item.name}>
+                        <Link className="nav-link" to={`/${item.path}`}>
+                            <span>
+                                {item.name}
+                            </span>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+            <AddPost />
+        </div>
+    </nav>
+);
+
+Categories.propTypes = {
+    categories: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        path: PropTypes.string.isRequired,
+    })).isRequired
+};
+
+const mapStateToProps = state => ({
+    categories: state.categories || []
+});
+
+const ConnectedCategories = connect(
+    mapStateToProps,
+)(Categories);
+
+export default ConnectedCategories;

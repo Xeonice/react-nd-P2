6d1dc7d47f698d27966c09d763cdadbe8497d8e9
Xeonice <ad546971975@gmail.com>
2018-03-14 14:30:31 +0800

    initialize the project, added action and reducer

---
+++ src/action/index.js
@@ -0,0 +1,64 @@
+import {
+    GET_ALL_POSTS,
+    GET_ALL_POST_COMMENTS,
+    GET_CATEGORIES,
+    GET_CURRENT_POST,
+    GET_POSTS_BY_CATEGORY
+} from "../constants";
+import * as BlogAPI from '../service/api'
+
+export const getCategories = categories => ({
+    type: GET_CATEGORIES,
+    categories,
+});
+export const getAllPosts = posts => ({
+    type: GET_ALL_POSTS,
+    posts,
+});
+export const getPostsByCategory = data => ({
+    type: GET_POSTS_BY_CATEGORY,
+    categoryPosts: data.categoryPosts,
+    name: data.name,
+});
+export const getPostComments = (postId, postComments) => ({
+    type: GET_ALL_POST_COMMENTS,
+    postId,
+    postComments,
+});
+export const getCurrentPost = currentData => ({
+    type: GET_CURRENT_POST,
+    currentData,
+});
+export const fetchCategories = () => dispatch => (
+    BlogAPI
+        .getAllCategories()
+        .then(data => dispatch(getCategories(data)))
+);
+export const fetchAllPosts = () => dispatch => (
+    BlogAPI
+        .getAllPosts()
+        .then(data => dispatch(getAllPosts({ posts: data })))
+);
+export const fetchPostsByCategory = category => dispatch => (
+    BlogAPI
+        .getPostsByCategory(category)
+        .then((data) => {
+            const response = {
+                categoryPosts: data,
+                name: category,
+            };
+            return dispatch(getPostsByCategory(response));
+        })
+);
+
+export const fetchPostComments = postId => dispatch => (
+    BlogAPI
+        .getPostComments(postId)
+        .then(response => dispatch(getPostComments(postId, response)))
+);
+
+export const fetchCurrentPost = postId => dispatch => (
+    BlogAPI
+        .getPost(postId)
+        .then(response => dispatch(getCurrentPost(response)))
+);
